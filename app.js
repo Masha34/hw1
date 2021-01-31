98bd732a02f57e88b8b65f745321204ecc8934b8
Maria Savchuk <savchuk_maria@ukr.net>
2021-01-31 20:26:44 +0200

    create express routes

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-console.log("work")
-console.log("work")
+const express = require("express")
+//  console.log(express)
+
+// Підключаємо shop routes
+const hwRoutes = require("./routes/hwRoutes");
+
+const PORT = 5555;
+const app = express();
+
+app.use(hwRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
